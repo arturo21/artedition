64458c02c60c2d63b7e7f1c9d2dbafa949b1d61e
Arturo Vasquez <artav17@gmail.com>
2019-10-20 12:40:15 -0400

    cambios REPO

---
+++ plugins/kcfinder/config.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+/** This file is part of KCFinder project
+  *
+  *      @desc Base configuration file
+  *   @package KCFinder
+  *   @version 2.51
+  *    @author Pavel Tzonkov <pavelc@users.sourceforge.net>
+  * @copyright 2010, 2011 KCFinder Project
+  *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
+  *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
+  *      @link http://kcfinder.sunhater.com
+  */
+
+// IMPORTANT!!! Do not remove uncommented settings in this file even if
+// you are using session configuration.
+// See http://kcfinder.sunhater.com/install for setting descriptions
+//Determinar si esta en entorno de prueba o produccion
+$_SESSION['url_entorno']=$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+//HTTP para los comentarios de facebook
+$url_entorno_real="http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+$url_entorno_real_c="http://".$_SERVER["SERVER_NAME"];
+$array_test=explode("/",$_SESSION['url_entorno']);
+//echo($array_test[0]);echo($array_test[1]);echo($array_test[2]);
+//Establecer tipo de entorno 
+//$tipoentorno=strtoupper($array_test[1]);
+$URLabs=$url_entorno_real_c."/ArtEditor/plugins/kcfinder/";
+$URLrel=$URLabs."upload/";
+$URLdos="upload/";
+
+$_CONFIG = array(
+
+    'disabled' => false,
+    'denyZipDownload' => false,
+    'denyUpdateCheck' => false,
+    'denyExtensionRename' => false,
+
+    'theme' => "oxygen",
+
+    'uploadURL' => $URLrel,
+    'uploadDir' => $URLdos,
+
+    'dirPerms' => 0777,
+    'filePerms' => 0777,
+
+    'access' => array(
+
+        'files' => array(
+            'upload' => true,
+            'delete' => true,
+            'copy' => true,
+            'move' => true,
+            'rename' => true
+        ),
+
+        'dirs' => array(
+            'create' => true,
+            'delete' => true,
+            'rename' => true
+        )
+    ),
+
+    'deniedExts' => "exe com msi bat php phps phtml php3 php4 cgi pl",
+
+    'types' => array(
+
+        // CKEditor & FCKEditor types
+        'files'   =>  "",
+        'flash'   =>  "swf",
+        'images'  =>  "*img",
+		'mimages'   => "*mime image/gif image/png image/jpeg",
+        // TinyMCE types
+        'file'    =>  "",
+        'media'   =>  "swf flv avi mpg mpeg qt mov wmv asf rm",
+        'image'   =>  "*img",
+		'mimages'   => "*mime image/gif image/png image/jpeg"
+    ),
+
+    'filenameChangeChars' => array(/*
+        ' ' => "_",
+        ':' => "."
+    */),
+
+    'dirnameChangeChars' => array(/*
+        ' ' => "_",
+        ':' => "."
+    */),
+
+    'mime_magic' => "",
+
+    'maxImageWidth' => 0,
+    'maxImageHeight' => 0,
+
+    'thumbWidth' => 100,
+    'thumbHeight' => 100,
+
+    'thumbsDir' => ".thumbs",
+
+    'jpegQuality' => 90,
+
+    'cookieDomain' => "",
+    'cookiePath' => "",
+    'cookiePrefix' => 'KCFINDER_',
+
+    // THE FOLLOWING SETTINGS CANNOT BE OVERRIDED WITH SESSION CONFIGURATION
+    '_check4htaccess' => true,
+    //'_tinyMCEPath' => "/tiny_mce",
+
+    '_sessionVar' => &$_SESSION['KCFINDER'],
+    //'_sessionLifetime' => 30,
+    //'_sessionDir' => "/full/directory/path",
+
+    //'_sessionDomain' => ".mysite.com",
+    //'_sessionPath' => "/my/path",
+);
+
+?>
